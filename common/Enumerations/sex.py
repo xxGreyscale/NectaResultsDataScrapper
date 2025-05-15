07bee71ffa85aa3ff13d97d3d3700ecca52f4b85
Salum Nassor <salim.nassor.sn@gmail.com>
2025-05-15 03:21:00 +0300

    improve: Added none as a valid enum

--- common/Enumerations/sex.py
+++ common/Enumerations/sex.py
@@ -4,6 +4,7 @@ from enum import Enum
 class SexEnum(str, Enum):
     MALE = ("M", "Male")
     FEMALE = ("F", "Female")
+    NONE = ("None", "None")  # No sex defined
 
     def __new__(cls, value, label):
         member = str.__new__(cls, value)
@@ -13,4 +14,11 @@ class SexEnum(str, Enum):
 
     def __str__(self):
         return self.label
+
+    def __eq__(self, other):
+        if isinstance(other, str):
+            return self._value_ == other
+        elif isinstance(other, SexEnum):
+            return self._value_ == other._value_
+        return False
 
