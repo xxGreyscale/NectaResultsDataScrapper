12ca4c3baba28329f21922c44f738969fe2e5255
Salum Nassor <salim.nassor.sn@gmail.com>
2025-05-16 02:59:54 +0300

    improve: map meta data to document and domain

--- application/center/services/center_mappers.py
+++ application/center/services/center_mappers.py
@@ -2,6 +2,7 @@ from application.center.repository.entities.center import CenterDocument, Change
     CurrentCenterData
 from common.Domain.centersummary import CenterSummary
 from common.Primitives.center_id import CenterId
+from common.Domain.meta_data import Metadata
 
 DATA_VERSION = 1.0  # change everytime we update data structure during development
 
@@ -21,12 +22,13 @@ class CenterMapper:
                 council=center_summary.council,
                 ward=center_summary.ward,
                 ownership=center_summary.ownership,
-                institutionType=center_summary.institution_type
+                institutionType=center_summary.institution_type,
+                metadata=[meta_data for meta_data in center_summary.meta_data] if center_summary.meta_data else None,
             ),
             created_at=center_summary.created_at,
             updated_at=center_summary.updated_at,
             data_version=DATA_VERSION,
-            change_logs=[change_log.to_dict() for change_log in changeLogs]
+            change_logs=[change_log for change_log in changeLogs],
         )
 
     @staticmethod
@@ -40,5 +42,8 @@ class CenterMapper:
             council=center_document.current.council,
             ward=center_document.current.ward,
             ownership=center_document.current.ownership,
-            institution_type=center_document.current.institutionType
+            institution_type=center_document.current.institutionType,
+            created_at=center_document.created_at,
+            updated_at=center_document.updated_at,
+            meta_data=[Metadata.from_dict(item) for item in center_document.metadata] if center_document.metadata else None,
         )
