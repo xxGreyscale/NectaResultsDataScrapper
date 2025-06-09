65fa5c11490613e0817a6cb3fa8684297b630921
Salum Nassor <salim.nassor.sn@gmail.com>
2025-06-09 15:11:50 +0300

    improve: improved content parser with year since different years have confusing results

--- application/parser/content_parser_mapper.py
+++ application/parser/content_parser_mapper.py
@@ -4,8 +4,9 @@ import re
 
 class ContentParserMapper:
     @staticmethod
-    def parse_table(table: html) -> list:
+    def parse_table(table: html, year: int) -> list:
         # Get headers first, the first row is the headers
+        # Adding year hear is not a good idea but inconsistencies within the result pages appear to be through years
         headers = []
         table_object = []
         for index, row in enumerate(table.find_all('tr')):
@@ -26,7 +27,7 @@ class ContentParserMapper:
 
                     if cell_text:
                         if headers[row_index] == 'DETAILED SUBJECTS':
-                            row_data[headers[row_index]] = ContentParserMapper.detailed_subjects_mapper(cell_text)
+                            row_data[headers[row_index]] = ContentParserMapper.detailed_subjects_mapper(cell_text, year)
                         else:
                             row_data[headers[row_index]] = cell_text
                 if row_data:
@@ -34,14 +35,20 @@ class ContentParserMapper:
         return table_object
 
     @staticmethod
-    def detailed_subjects_mapper(detailed_subjects: str) -> dict:
+    def detailed_subjects_mapper(detailed_subjects: str, year: int) -> dict:
         """"
         This is specific for the NECTA result page table
+        :param year: 
         :param detailed_subjects: The detailed subjects string
         :return: A dictionary with subject names as keys and grades as values
         """""
         # Regular expression pattern to find subject and grade
-        pattern = r"([a-zA-Z0-9\s/-]+) - '([A-Z/]+)'"
+        if year < 2009:
+            pattern = r"([a-zA-Z0-9\s&.]+)-([A-Z/]{1,2}|[A-Z/]{3}(?=[^ ]))"  # Per the result page
+        elif 2014 > year > 2008:
+            pattern = r"([a-zA-Z0-9/]+)-([A-Z])(?:\s|$)"  # Per the result page
+        else:
+            pattern = r"([a-zA-Z0-9\s/-]+) - '([A-Z/]+)'"  # Per the result page
         # Find all matches of the pattern in the string
         matches = re.findall(pattern, detailed_subjects)
         # Convert the list of tuples (subject, grade) to a dictionary
