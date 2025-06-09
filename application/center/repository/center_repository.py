36ea47455dd969ea8a2d7d1494684bef3916bc1f
Salum Nassor <salim.nassor.sn@gmail.com>
2025-06-09 16:18:08 +0300

    refactor: lean code (transferred to DB setup)

--- application/center/repository/center_repository.py
+++ application/center/repository/center_repository.py
@@ -1,21 +1,15 @@
-from collections.abc import Mapping
 from http.client import HTTPException
-
-from pymongo.synchronous.collection import Collection
 from application.center.repository.entities.center import CenterDocument
 from infastructure.database_config import get_database
 
 
 class CenterRepository:
     def __init__(self):
-
         try:
             db = get_database("mongodb://root:admin@localhost:27018/", "necta")
             self.collection = db["necta_centers"]
         except Exception as e:
             RuntimeWarning(f"Error initializing CenterRepository: {e}")
-        self.make_necta_reg_no_unique_key(self.collection)
-        # self.make_school_reg_no_unique_key(self.collection)
 
     def get_all_centers(self):
         return self.collection.find()
@@ -63,22 +57,3 @@ class CenterRepository:
             raise HTTPException()
         # Find a better response
         return "deleted"
-
-    @staticmethod
-    def make_school_reg_no_unique_key(_db):
-        try:
-            _db.create_index("schoolRegistrationNo", unique=True)
-            print("Unique index created on schoolRegistrationNo.")
-        except Exception as e:
-            print(f"Error creating unique index on schoolRegistrationNo: {e}")
-            RuntimeWarning("Error creating unique index on nectaRegistrationNo: {e}")
-
-    @staticmethod
-    def make_necta_reg_no_unique_key(_db: Collection[Mapping[str, any]]):
-        try:
-            # Create a unique index on the 'nectaRegistrationNo' field
-            _db.create_index("identifiers.nectaRegistrationNo", unique=True)
-            print("Unique index created on nectaRegistrationNo.")
-        except Exception as e:
-            print(f"Error creating unique index on nectaRegistrationNo: {e}")
-            RuntimeWarning("Error creating unique index on nectaRegistrationNo: {e}")
