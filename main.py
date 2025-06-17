5511740e9fbd1adf0422d32af234242485276227
Salum Nassor <salim.nassor.sn@gmail.com>
2025-06-18 02:34:53 +0300

    feat: added a argument parsers for command line like experience

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from infastructure.database_config import close_db_client
+from infastructure.setup_database import DbSetup
+from usecases.get_acsee_summary_results import results_summary_csv
 from usecases.get_necta_results import get_and_save_acsee_results
+import argparse
 
 
-def get_table():
+def get_necta_acsee_results(start_year: int = 2022, end_year: int = 2024):
     url = "https://maktaba.tetea.org/exam-results/"
-    years = [year for year in range(2023, 2024)]
+    years = [year for year in range(start_year, end_year)]
+    DbSetup().setup_indices()
     get_and_save_acsee_results(url, "ACSEE", years)
     close_db_client()
 
@@ -12,7 +16,55 @@ def get_table():
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     # Make sure db is available
-    get_table()
-    # get_excel('resource/2004/enrolment/Secondary_by_Age_and_Sex.xlsx')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    results_summary_csv() # Remove after debugging
+    # parser = argparse.ArgumentParser(
+    #     description="A script to process NECTA data, generate CSVs, or extract Excel data."
+    # )
+    # # Optional argument to run results_summary_csv
+    # parser.add_argument(
+    #     '--results-summary-csv',
+    #     action='store_true',
+    #     help='Generate the results summary CSV file.'
+    # )
+    #
+    # parser.add_argument(
+    #     '--get-acsee-results',
+    #     action='store_true',
+    #     help='Run the function to fetch and save ACSEE exam results.'
+    # )
+    #
+    # parser.add_argument(
+    #     '--get-table',
+    #     action='store_true',  # If this flag is present, its value will be True
+    #     help='Run the get_necta_acsee_results() and get then save '
+    #          'Necta acsee results in the database. For specified years'
+    # )
+    #
+    # # Arguments for start_year and end_year, dependent on --get-acsee-results
+    # parser.add_argument(
+    #     '--start-year',
+    #     type=int,
+    #     default=2022,  # Default value if not provided
+    #     help='Specify the start year for fetching ACSEE results (e.g., 2022). Only applicable with --get-acsee-results.'
+    # )
+    # parser.add_argument(
+    #     '--end-year',
+    #     type=int,
+    #     default=2024,  # Default value if not provided
+    #     help='Specify the end year for fetching ACSEE results (inclusive, e.g., 2024). Only applicable with '
+    #          '--get-acsee-results.'
+    # )
+    #
+    # args = parser.parse_args()
+    #
+    # if args.results_summary_csv:
+    #     results_summary_csv()
+    # if args.get_acsee_results:
+    #     if args.start_year is None or args.end_year is None:
+    #         print("Error: start_year and end_year must be specified with --get-acsee-results.")
+    #     if args.start_year >= args.end_year:
+    #         print("Error: start_year must be less than end_year.")
+    #     get_necta_acsee_results(args.start_year, args.end_year)
+    #
+    # if not any(vars(args).values()):  # Check if any argument was provided
+    #     print("No specific action requested. Please use --help for options.")
